20b2d9f00a128b6372662a72fdf6a8ebf1f4cdc2
Axel CARNEZ <axel.carnez@efrei.net>
2020-11-08 15:52:39 +0100

    Ajusting parameters

--- webinterface/app.py
+++ webinterface/app.py
@@ -1,31 +1,23 @@
 from flask import Flask, request, render_template
-from flask_restful import reqparse, abort, Api, Resource
 import pickle
-from pathlib import Path
-
-
-path = Path(".")
-
 
 app = Flask(__name__)
 
-api = Api(app)
 
-with open(path.parent+ '/model-learning/vectorizer.pkl', 'rb') as f:
+with open('../model-learning/vectorizer.pkl', 'rb') as f:
 	vectorizer = pickle.load(f)
 
-with open(path.parent+ '/model-learning/sentiment_classifier.pkl','rb') as f:
+with open('../model-learning/sentiment_classifier.pkl','rb') as f:
 	sentiment_classifier = pickle.load(f)
 	
 
-
-class PredictSentiment(Resource):
-	def machineLearningPrediction(phrase):
-		user_input = vectorizer.transform([str(phrase)])
-		prediction = sentiment_classifier.predict(user_input)
-		result = lambda x: 'Negative' if prediction == 0 else ('Neutral' if prediction == 1 else 'Positive') 
-		output = {'Prediction': result}
-		return output
+def machineLearningPrediction(phrase):
+	print("## Phrase : "+ phrase+" ##")
+	user_input = vectorizer.transform([str(phrase)])
+	prediction = sentiment_classifier.predict(user_input)
+	#result = lambda x: 'Negative' if prediction == 0 else ('Neutral' if prediction == 1 else 'Positive') 
+	output = {'Prediction': prediction[0]}
+	return output
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
