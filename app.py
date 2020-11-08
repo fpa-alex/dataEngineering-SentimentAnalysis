e92b5bc46bd204d7cb83f269d9bdac1570b47b18
Axel CARNEZ <axel.carnez@efrei.net>
2020-11-08 11:49:41 +0100

    The app can respond to the form

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+def machineLearningPrediction(phrase) :
+    return "My response"
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST' :
+        details = request.form
+        result = machineLearningPrediction(details['phrase'])
+        return render_template('index.html', result=result)
+        
+    else :
+        return render_template('index.html')
 
 if __name__ == 'main':
     app.run(host='0.0.0.0')
+
